4f339292367a548951aaf35141dc74015b8e08ab
Annie Cypcar <annecypcar@gmail.com>
2023-01-12 08:43:13 -0500

    fixing bounds

--- fizzbuzz/main.py
+++ fizzbuzz/main.py
@@ -8,7 +8,7 @@ def fizzbuzz(request):
     num = request_json['input']
     output = []
     num_int = int(num)
-    if num_int > 1 and num_int < 1000:
+    if num_int >= 1 and num_int <= 1000:
         output = [i+1 for i in range(num_int)]
         for i in range(len(output)):
             if output[i] % 5 == 0 and output[i] % 3 == 0:
